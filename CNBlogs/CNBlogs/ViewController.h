eac3e890c445a99eaed6a0e341c2730122550971
liyuanchao <liyuanchao@xiaoyezi.com>
2015-08-25 19:37:48 +0800

    init cnblogs

---
+++ CNBlogs/CNBlogs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CNBlogs
+//
+//  Created by 李远超 on 15/8/25.
+//  Copyright (c) 2015年 liyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CNBlogs/CNBlogs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CNBlogs
+//
+//  Created by 李远超 on 15/8/25.
+//  Copyright (c) 2015年 liyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
